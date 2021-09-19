504ca896b1d2c93eaa1c22ce501b5897cef87494
Hopanav007 <54102139+Hopanav007@users.noreply.github.com>
2021-09-19 15:46:42 +0600

    Delete task1.py

--- task1.py
+++
@@ -1,7 +0,0 @@
-import itertools
- 
-if __name__ == '__main__':
-    symbols = 'bac'
-    string = list(symbols)
-    permutations = list(itertools.permutations(string))
-    print([''.join(permutation) for permutation in permutations])
